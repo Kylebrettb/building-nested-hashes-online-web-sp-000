3b72fa177cc84fc2a484499768a425bec95fe87f
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-12-18 18:35:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,22 +1,6 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
-end
-
-
-
-
-
-def second_challenge
-  epic_tragedy = {
-    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
